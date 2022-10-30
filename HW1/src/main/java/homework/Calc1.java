614428d8111c1346c55b6b182bc3f9326d2cdb01
EugeneHH <zenadegtarev766@gmail.com>
2022-10-30 15:59:34 +0000

    Calc1-finish

---
+++ HW1/src/main/java/homework/Calc1.java
@@ -0,0 +1,19 @@
+package homework;
+import java.util.Scanner;
+
+public class Calc1 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        //Прием данных из консоли
+        System.out.println("Input first number");
+        int a = scanner.nextInt();
+        System.out.println(a +" - First number");
+        System.out.println("Input second number");
+        int b = scanner.nextInt();
+        System.out.println(b +" - Second number");
+        int c = (a + b)/2;
+        System.out.println("Answer: "+ c);
+        //Нахождение средне арифметического числа из 2-ух чисел
+    }
+}
