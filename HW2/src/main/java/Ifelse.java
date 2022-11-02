bcbb7883abf5916626aa588d1eaaa31805c3041c
EugeneHH <zenadegtarev766@gmail.com>
2022-11-02 17:00:20 +0000

    homework2if

---
+++ HW2/src/main/java/Ifelse.java
@@ -0,0 +1,13 @@
+public class Ifelse {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        if (a > b) {//в случае если а больше б мы выводим
+            System.out.println("A is a greater than B");//первый вариант
+        } else {//или мы выводим
+            System.out.println("B is greater");//второй вариант
+        }
+    }
+}
+//  if - можно перевести как если
